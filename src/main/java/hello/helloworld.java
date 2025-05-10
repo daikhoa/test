97b8b3df5b0b015d55122e9d6ef21a85a8a1abcd
daikhoa <nguyendaikhoa010104@gmail.com>
2025-05-10 16:10:44 +0700

    sửa

--- src/main/java/hello/helloworld.java
+++ src/main/java/hello/helloworld.java
@@ -2,7 +2,7 @@ package hello;
 
 public class helloworld {
 	public static String sayHello() {
-        return "HelloWord";
+        return "HelloWorld";
     }
 
     public static void main(String[] args) {
